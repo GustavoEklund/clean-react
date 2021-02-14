295bb4863bcf3ae19747e8a3e05efcbc3dcf7dfc
Gustavo Eklund <gustavo04616@gmail.com>
2021-02-14 20:47:25 -0300

    feat: ensure Login link goes to the right place

--- src/presentation/pages/signup/signup.tsx
+++ src/presentation/pages/signup/signup.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { useHistory } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
 import Styles from './signup-styles.scss'
 import Context from '@/presentation/contexts/form/form-context'
 import { LoginHeader, Footer, Input, FormStatus } from '@/presentation/components'
@@ -78,7 +78,7 @@ const SignUp: React.FC<Props> = ({ validation, addAccount, saveAccessToken }: Pr
           <Input type="password" name="password" placeholder="Digite sua senha" />
           <Input type="password" name="passwordConfirmation" placeholder="Repita sua senha" />
           <button data-testid="submit" disabled={!!state.nameError || !!state.emailError || !!state.passwordError || !!state.passwordConfirmationError} className={Styles.submit} type="submit">Cadastrar</button>
-          <span className={Styles.link}>Já tenho uma conta</span>
+          <Link data-testid="login-link" to="/login" className={Styles.link}>Já tenho uma conta</Link>
           <FormStatus />
         </form>
       </Context.Provider>
