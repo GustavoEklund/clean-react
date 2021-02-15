cb0f2e327d96def8d8e5ae80e04bcbb49ed2aee1
Gustavo Eklund <gustavo04616@gmail.com>
2021-02-14 22:59:58 -0300

    feat: add SignUp Composition

--- src/main/factories/pages/signup/signup-validation-factory.spec.ts
+++ src/main/factories/pages/signup/signup-validation-factory.spec.ts
@@ -6,10 +6,10 @@ describe('SignUpValidationFactory', () => {
   test('Should compose ValidationComposite with correct validations', () => {
     const composite = makeSignUpValidation()
     expect(composite).toEqual(ValidationComposite.build([
-      ...Builder.field('min').required().min(3).build(),
+      ...Builder.field('name').required().min(3).build(),
       ...Builder.field('email').required().email().build(),
       ...Builder.field('password').required().min(5).build(),
-      ...Builder.field('passwordConfirmation').required().min(5).build()
+      ...Builder.field('passwordConfirmation').required().sameAs('password').build()
     ]))
   })
 })
--- src/main/factories/pages/signup/signup-validation-factory.ts
+++ src/main/factories/pages/signup/signup-validation-factory.ts
@@ -3,9 +3,9 @@ import { ValidationBuilder as Builder } from '@/validation/validators/builder/va
 
 export const makeSignUpValidation = (): ValidationComposite => {
   return ValidationComposite.build([
-    ...Builder.field('min').required().min(3).build(),
+    ...Builder.field('name').required().min(3).build(),
     ...Builder.field('email').required().email().build(),
     ...Builder.field('password').required().min(5).build(),
-    ...Builder.field('passwordConfirmation').required().min(5).build()
+    ...Builder.field('passwordConfirmation').required().sameAs('password').build()
   ])
 }
